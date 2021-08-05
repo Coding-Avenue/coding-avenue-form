a2db98a0e6b4b25bd738d8c4e1d3445a6d7a92bd
Kuldeep Songara <kuldeepsongara@Kuldeeps-MacBook-Pro.local>
2021-08-05 08:54:54 +0530

    Browser UI Completed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,6 @@ import CavenueForm from "../../components/CavenueForm";
 import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import logo from "../../assets/logo.png";
-import logoMobile from "../../assets/logoMobile.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -38,10 +37,34 @@ const useStyles = makeStyles((theme) => ({
   mLogo: {
     width: 250,
   },
+  browser: {
+    display: "flex",
+  },
   banner: {
     backgroundColor: "#36473D",
     minHeight: "100vh",
     width: "40%",
+  },
+  formContainer: {
+    width: "60%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  form: {
+    maxWidth: 650,
+  },
+  logoContainer: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: "120%",
+    height: "auto",
+    transform: "rotate(-90deg)",
   },
 }));
 
@@ -67,7 +90,7 @@ const Home = (props) => {
     return (
       <div className={classes.mobile}>
         <Box className={classes.mLogoContainer}>
-          <img src={logo} className={classes.mLogo} />
+          <img src={logo} alt="Brand Logo" className={classes.mLogo} />
         </Box>
         <Box className={classes.mForm}>
           <Box className={classes.mChildren}>
@@ -79,9 +102,15 @@ const Home = (props) => {
   }
   return (
     <div className={classes.browser}>
-      <Box className={classes.banner}></Box>
-      <Box className={classes.form}>
-        <CavenueForm />
+      <Box className={classes.banner}>
+        <Box className={classes.logoContainer}>
+          <img src={logo} alt="Brand Logo" className={classes.logo} />
+        </Box>
+      </Box>
+      <Box className={classes.formContainer}>
+        <Box className={classes.form}>
+          <CavenueForm />
+        </Box>
       </Box>
     </div>
   );
